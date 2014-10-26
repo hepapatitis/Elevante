5b6d31f3a6e4d19d6268c7bd2da559b98aa899ca
hepapatitis <hepapatitis@yahoo.com>
2014-10-27 08:47:09 +1100

    Updater

--- code/js/main.js
+++ code/js/main.js
@@ -1,19 +1,4 @@
 $(document).ready(function() {
-	// Easing for #hashtags
-	$('a[href*=#]:not([href=#])').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-			|| location.hostname == this.hostname) {
-
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			
-			if (target.length) {
-				$('html,body').animate({scrollTop: target.offset().top}, 1500, 'easeInOutQuint');
-				return false;
-			}
-		}
-	});
-	
 	// Magnific Popup
 	$('.magnific-popup').magnificPopup({
 		gallery: {
